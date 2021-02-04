d3589380faaaded8b84cadd9a4195f0b28667ff4
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-02-04 14:29:01 +0000

    Add pruning for intent detection

--- transformerlab/pruning.py
+++ transformerlab/pruning.py
@@ -2,7 +2,8 @@
 
 __all__ = ['MaskedBertConfig', 'MaskedBertPreTrainedModel', 'MaskedBertModel', 'BertEmbeddings', 'BertEncoder',
            'MaskedBertForQuestionAnswering', 'BertLayer', 'BertAttention', 'BertSelfAttention', 'MaskedLinear',
-           'BertSelfOutput', 'BertIntermediate', 'BertOutput', 'BertPooler', 'TopKBinarizer']
+           'BertSelfOutput', 'BertIntermediate', 'BertOutput', 'BertPooler', 'TopKBinarizer',
+           'MaskedBertForSequenceClassification']
 
 # Cell
 import math
@@ -778,3 +779,56 @@ class TopKBinarizer(autograd.Function):
     @staticmethod
     def backward(ctx, gradOutput):
         return gradOutput, None
+
+# Cell
+class MaskedBertForSequenceClassification(MaskedBertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+
+        self.bert = MaskedBertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        threshold=None,
+    ):
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            threshold=threshold,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+
+        if labels is not None:
+            if self.num_labels == 1:
+                #  We are doing regression
+                loss_fct = MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            outputs = (loss,) + outputs
+
+        return outputs  # (loss), logits, (hidden_states), (attentions)
